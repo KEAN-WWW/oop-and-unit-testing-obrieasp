da4db768d69cee7107d03b3136a448caa1b63df0
obrieasp <obrieasp@kean.edu>
2025-02-23 20:17:40 -0500

    Submit

---
+++ app/calculator.py
@@ -0,0 +1,16 @@
+class Calculator(object):
+    @staticmethod
+    def addition(val1, val2):
+        return val1 + val2
+
+    @staticmethod
+    def subtraction(self, val1, val2):
+        return val1 - val2
+
+    @staticmethod
+    def multiply(self, val1, val2):
+        return val1 * val2
+
+    @staticmethod
+    def divide(self, val1, val2):
+        return val1 / val2
